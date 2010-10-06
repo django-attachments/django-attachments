5e2decc5dbcee895272af086971b32ff98786a19
Christian Oudard <christian.oudard@gmail.com>
2010-10-06 13:42:57 -0400

    Added an order_by option to the get_attachments template tag.

--- attachments/templatetags/attachment_tags.py
+++ attachments/templatetags/attachment_tags.py
@@ -20,21 +20,32 @@ def new_attachment_url(content_object):
     return reverse('attachment_new', kwargs=kwargs)
 
 class ObjectAttachmentsNode(template.Node):
-    def __init__(self, content_object, context_name):
+    def __init__(self, content_object, context_name, order_by):
         self.content_object = template.Variable(content_object)
         self.context_name = context_name
+        self.order_by = order_by
     def render(self, context):
         content_object = self.content_object.resolve(context)
-        context[self.context_name] = Attachment.objects.attachments_for_object(content_object)
+        attachments = Attachment.objects.attachments_for_object(content_object)
+        if self.order_by:
+            attachments = attachments.order_by(self.order_by)
+        context[self.context_name] = attachments
         return ''
 
 def do_get_attachments(parser, token):
-    error_string = "%r tag must be of format {%% get_attachments for OBJECT as CONTEXT_VARIABLE %%}" % token.contents.split()[0]
-    try:
-        tag, word_for, content_object, word_as, context_name = token.split_contents()
-    except ValueError:
+    bits = token.contents.split()
+    error_string = "%r tag must be of format {%% get_attachments for OBJECT as CONTEXT_VARIABLE %%}" % bits[0]
+    order_by = None
+    if len(bits) == 5:
+        try:
+            tag, word_for, content_object, word_as, context_name = bits
+        except ValueError:
+            raise template.TemplateSyntaxError(error_string)
+    elif len(bits) == 7:
+        tag, word_for, content_object, word_as, context_name, word_order_by, order_by = bits
+    else:
         raise template.TemplateSyntaxError(error_string)
-    return ObjectAttachmentsNode(content_object, context_name)
+    return ObjectAttachmentsNode(content_object, context_name, order_by)
 
 
 register = template.Library()
