2b0dd368571996c8218e626236d3cc0dfb7b007f
Jason Ward <jason.ward@policystat.com>
2017-11-16 22:47:27 -0500

    refs #5: Looks like url patterns are not a thing anymore? Good to know.

--- attachments/urls.py
+++ attachments/urls.py
@@ -1,23 +1,29 @@
-try:
-    from django.conf.urls import patterns, url
-except ImportError:  # Django 1.4
-    from django.conf.urls.defaults import patterns, url
+from django.conf.urls import url
 from django.contrib import admin
+
+import attachments.views
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    'attachments.views',
-    url(r'^(?P<content_type>\d+)/(?P<object_id>\d+)/$',
-        'list_attachments',
-        name='attachment_list'),
-    url(r'^(?P<content_type>\d+)/(?P<object_id>\d+)/new/$',
-        'new_attachment',
-        name='attachment_new'),
-    url(r'^(?P<attachment_id>\d+)/edit/$',
-        'edit_attachment',
-        name='attachment_edit'),
-    url(r'^(?P<attachment_id>\d+)/delete/$',
-        'delete_attachment',
-        name='attachment_delete'),
+urlpatterns = (
+    url(
+        r'^(?P<content_type>\d+)/(?P<object_id>\d+)/$',
+        attachments.views.list_attachments,
+        name='attachment_list',
+    ),
+    url(
+        r'^(?P<content_type>\d+)/(?P<object_id>\d+)/new/$',
+        attachments.views.new_attachment,
+        name='attachment_new',
+    ),
+    url(
+        r'^(?P<attachment_id>\d+)/edit/$',
+        attachments.views.edit_attachment,
+        name='attachment_edit',
+    ),
+    url(
+        r'^(?P<attachment_id>\d+)/delete/$',
+        attachments.views.delete_attachment,
+        name='attachment_delete',
+    ),
 )
